87ecdfc9cf77e514b0b33e64a521b42834dafdc4
defihook <kevinjakeedward@gmail.com>
2022-05-03 14:49:00 +0200

    Add more params

--- src/app.ts
+++ src/app.ts
@@ -11,14 +11,41 @@ const openai = new OpenAIApi(configuration);
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
-client.on("messageCreate", async function(message) {
+let lastRequest: CreateCompletionRequest = {
+    prompt: "hello",
+    max_tokens: 10,
+    temperature: 1,
+    presence_penalty: 0,
+    frequency_penalty: 0.3,
+    best_of: 1,
+    n: 1,
+    stream: false,
+};
+
+let lastResponse = "";
+
+client.on("messageCreate", async function (message) {
     if (message.author.bot) return;
     if (!message.content.startsWith("!gpt3")) return;
+    if (message.content.startsWith("!gpt3 ...")) {
+        lastRequest.prompt = lastRequest.prompt + '\n' + lastResponse;
+        // @ts-ignore
+        lastRequest.max_tokens = lastRequest.max_tokens + 128;
+        console.log(lastRequest);
+        const gptResponse = await openai.createCompletion("text-davinci-002", lastRequest);
+        // @ts-ignore
+        lastResponse = gptResponse.data.choices[0].text || "Fin.";
+
+        await message.reply(`${lastResponse}`);
+        return;
+    }
 
     const args = message.content.split('\n')[0].split(' ');
-    const prompt = message.content.substring(message.content.indexOf("\n") + 1) ;
+    const prompt = message.content.substring(message.content.indexOf("\n") + 1);
     let temperature = 0.5;
     let max_tokens = 256;
+    let frequency_penalty = 0.3;
+    let presence_penalty = 0;
 
     if (args.length >= 2) {
         temperature = Number(args[1]);
@@ -26,28 +53,37 @@ client.on("messageCreate", async function(message) {
     if (args.length >= 3) {
         max_tokens = Number(args[2]);
     }
+    if (args.length >= 4) {
+        frequency_penalty = Number(args[3]);
+    }
+    if (args.length >= 5) {
+        presence_penalty = Number(args[4]);
+    }
 
     const config: CreateCompletionRequest = {
         prompt: prompt,
         max_tokens: max_tokens,
         temperature: temperature,
-        presence_penalty: 0,
-        frequency_penalty: 0.3,
+        presence_penalty: presence_penalty,
+        frequency_penalty: frequency_penalty,
         best_of: 1,
         n: 1,
         stream: false,
     };
 
+    lastRequest = config;
+
     console.log(config);
-    
-    const gptResponse = await openai.createCompletion("text-davinci-002", 
-        config
-    )
+
+    const gptResponse = await openai.createCompletion("text-davinci-002", config);
+
     // @ts-ignore
-    await message.reply(`${gptResponse.data.choices[0].text}`);
- }); 
+    lastResponse = gptResponse.data.choices[0].text;
+
+    await message.reply(`${lastResponse}`);
+});
 
 
 client.login(process.env.BOT_TOKEN);
 
-    
+
